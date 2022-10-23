5fc05eac4ce10ca817dae926e40fcbdb27091552
Steb Veksler <steb@groundcover.com>
2022-10-23 07:33:41 +0000

    [sc-5148] proxy for loki push endpoint

---
+++ config/config.go
@@ -0,0 +1,66 @@
+package config
+
+import (
+	_ "embed"
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+//go:embed config.yaml
+var defaultConfig string
+
+type Config struct {
+	Target struct {
+		Url       string
+		TenantId  string `mapstructure:"tenant_id"`
+		LabelName string `mapstructure:"label_name"`
+	}
+	Bind struct {
+		Port    int
+		Address string
+	}
+}
+
+func NewConfig() *Config {
+	var config Config
+
+	viper.AutomaticEnv()
+	viper.SetConfigType("yaml")
+	viper.SetConfigName("config")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.ReadConfig(strings.NewReader(defaultConfig))
+
+	if err := viper.MergeInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			panic(err)
+		}
+	}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		panic(err)
+	}
+
+	return &config
+}
+
+func (config *Config) Print() {
+	fmt.Printf("config: %+v\n", config)
+}
+
+func (config *Config) BindAddr() string {
+	return fmt.Sprintf("%s:%d", config.Bind.Address, config.Bind.Port)
+}
+
+func (config *Config) TargetUrl() *url.URL {
+	var err error
+
+	var targetUrl *url.URL
+	if targetUrl, err = url.Parse(config.Target.Url); err != nil {
+		panic(err)
+	}
+
+	return targetUrl
+}
